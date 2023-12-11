61099437a83d39cf50787d8207cd7bcbbe7e398d
jjun11 <jok6961@gmail.com>
2023-12-11 17:00:34 +0900

    2023-12-11

---
+++ src/main/java/com/projectBackend/project/dto/PerformerDto.java
@@ -0,0 +1,13 @@
+package com.projectBackend.project.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PerformerDto {
+    private Long performaerId;
+    private String performanceId;
+    private String performers;
+
+}
