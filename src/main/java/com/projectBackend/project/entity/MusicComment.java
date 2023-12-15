3a13a45caaa0faf31b22d586724c77e2d34fe573
jjun11 <jok6961@gmail.com>
2023-12-15 19:26:18 +0900

    2023-12-15

---
+++ src/main/java/com/projectBackend/project/entity/MusicComment.java
@@ -0,0 +1,35 @@
+package com.projectBackend.project.entity;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@Table(name = "music_comment")
+public class MusicComment {
+
+    @Id
+    @Column(name = "musiccomment_id", length = 20)
+    private String musicCommentID;
+
+    @Column(name = "comment_content", length = 500, nullable = false)
+    private String commentContent;
+
+    @Column(name = "comment_Date", nullable = false)
+    private LocalDateTime commentDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_email", nullable = false)
+    private Member member;
+
+
+}
